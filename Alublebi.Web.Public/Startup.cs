335bfa4e6a7db10289c22833d417d610f7243b05
Irakli Murusidze <iraklimurusidze@live.com>
2017-03-20 17:51:38 +0400

    Add project files.

---
+++ Alublebi.Web.Public/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Alublebi.Web.Public.Startup))]
+namespace Alublebi.Web.Public
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
