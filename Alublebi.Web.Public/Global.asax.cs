a6bbff9f9b1ca6f1f46a484ca8653e129ab0dfe3
Irakli Murusidze <iraklimurusidze@live.com>
2017-04-19 20:48:07 +0400

    [Corrected structure] [Added some pages]

--- Alublebi.Web.Public/Global.asax.cs
+++ Alublebi.Web.Public/Global.asax.cs
@@ -15,7 +15,6 @@ namespace Alublebi.Web.Public
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
-            BundleConfig.RegisterBundles(BundleTable.Bundles);
         }
     }
 }
